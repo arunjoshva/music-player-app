af938fb30b810d2ae354c029dead18dd003ac20c
arunjoshva <arjose1993@gmail.com>
2026-05-14 13:20:21 +0530

    chore: initialize React TypeScript music player project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  
+  return (
+    <div className="min-h-screen bg-zinc-950 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Music Player App</h1>
+    </div>
+  )
+}
+
+export default App
