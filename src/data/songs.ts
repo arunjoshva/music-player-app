910e78cf6d5dff7b016af5011771104c66ca04b2
arunjoshva <arjose1993@gmail.com>
2026-05-15 11:43:44 +0530

    refactor: move audio assets to cloudinary

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -1,11 +1,5 @@
 import type { Song } from "../types/song";
 
-import song1 from "../assets/songs/afro-pop-music.mp3";
-import song2 from "../assets/songs/dance-playful-night.mp3";
-import song3 from "../assets/songs/gvidon-medicine.mp3";
-import song4 from "../assets/songs/honey-kisses.mp3";
-import song5 from "../assets/songs/upbeat-happy-corporate.mp3";
-
 import cover1 from "../assets/images/afro-pop.jpg";
 import cover2 from "../assets/images/dance-playful.jpg";
 import cover3 from "../assets/images/gvidon.jpg";
@@ -16,31 +10,31 @@ export const songs: Song[] = [
     {
         title: "Water | Afro-pop Music",
         artist: "Kontraa",
-        audioSrc: song1,
+        audioSrc: "https://res.cloudinary.com/dylitgp3e/video/upload/v1778825358/afro-pop-music_b3z9ly.mp3",
         imageSrc: cover1
     },
     {
         title: "Dance Playful Night",
         artist: "AleXZavesa",
-        audioSrc: song2,
+        audioSrc: "https://res.cloudinary.com/dylitgp3e/video/upload/v1778825261/dance-playful-night_pej7gj.mp3",
         imageSrc: cover2
     },
     {
         title: "Gvidon - Medicine",
         artist: "Gvidon",
-        audioSrc: song3,
+        audioSrc: "https://res.cloudinary.com/dylitgp3e/video/upload/v1778825311/gvidon-medicine_dljjvz.mp3",
         imageSrc: cover3
     },
     {
         title: "Honey Kisses",
         artist: "DeltaX-Music",
-        audioSrc: song4,
+        audioSrc: "https://res.cloudinary.com/dylitgp3e/video/upload/v1778825333/honey-kisses_obgtam.mp3",
         imageSrc: cover4
     },
     {
         title: "Upbeat Happy Corporate",
         artist: "kornevmusic",
-        audioSrc: song5,
+        audioSrc: "https://res.cloudinary.com/dylitgp3e/video/upload/v1778825312/upbeat-happy-corporate_oudd17.mp3",
         imageSrc: cover5
     }
 ];
